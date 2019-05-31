f1342520e17e9f051a566680b26ff02c0023768f
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-05-31 01:15:03 -0700

    completed reverse words in a string challenge

---
+++ reverse_words_in_a_string_iii.rb
@@ -0,0 +1,11 @@
+# Ruby 2.5.3
+# https://leetcode.com/problems/reverse-words-in-a-string-iii/submissions/
+
+def reverse_words(s)
+  output = []
+  words = s.split(" ")
+  words.each do |word|
+      output << word.reverse! 
+  end
+  return output.join(" ")
+end
