95bd40e122de60f1ffc253c923880cc081de4ae5
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-05-31 00:18:41 -0700

    completed self dividing numbers challenge

--- self_dividing_numbers.rb
+++ self_dividing_numbers.rb
@@ -1,3 +1,6 @@
+# Ruby 2.5.3
+# https://leetcode.com/problems/self-dividing-numbers/
+
 def self_dividing_numbers(left, right)
   output_nums = []
   (left..right).each do |num|
@@ -7,12 +10,24 @@ def self_dividing_numbers(left, right)
     # if num contains the digit zero, skip
     next if digits.include? '0'
     # loop through digits
+    digit_index = 0 # to keep track of digit position
     digits.each do |digit|
-      #test if num % digit is 0
+      if num % digit.to_i == 0
+        if digit_index == digits.length - 1 # check to see if this is the last digit
+          # if it is divisible by all digits, add num to output array
+          output_nums << num
+          break
+        end
+      else
+        # if num is not divisible by digit, move to next num
+        break
+      end
+      # increment index position for each iteration
+      digit_index += 1
     end
-    # if it passes all digits, add to output array
-    # else, skip and go to next num
   end
+  return output_nums
 end 
 
+# test
 self_dividing_numbers(1, 22)
