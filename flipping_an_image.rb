bc07b4ff70b6d87be57231a84955aa151111c13e
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-06-01 15:25:29 -0700

    completed flipping an image challenge

---
+++ flipping_an_image.rb
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/flipping-an-image/
+
+# a is an array containing arrays
+def flip_and_invert_image(a)
+  # loop through each array within main array
+  a.each do |array|
+      array.reverse!
+      # inline map!, if value is 1, subtract 1. Else, add 1.
+      array.map! { |value| value == 1 ? value -= 1 : value += 1 }
+  end
+end
