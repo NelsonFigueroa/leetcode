4797c5dd3af6753d12392571308f163fd310c3f6
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-08-31 13:31:44 -0700

    completed add two numbers challenge

---
+++ ruby/add_two_numbers.rb
@@ -0,0 +1,69 @@
+# Ruby 2.6.3
+# https://leetcode.com/problems/add-two-numbers/
+
+class ListNode
+	attr_accessor :val, :next
+
+	def initialize(val)
+		@val = val
+		@next = nil
+	end
+end
+
+# linked list 1, [2,4,3]
+l1 = ListNode.new(1)
+# l1.next = ListNode.new(4)
+# l1.next.next = ListNode.new(3)
+
+# linked list 2, [5,6,4]
+l2 = ListNode.new(9)
+l2.next = ListNode.new(9)
+# l2.next.next = ListNode.new(4)
+
+
+def add_two_numbers(l1, l2)
+	num1 = ''
+	num2 = ''
+
+	# get number from first linked list
+	loop do
+		node = l1
+		num1.concat(node.val.to_s)
+	  break if l1.next.nil?
+	  l1 = l1.next
+	end 
+
+	# get number from second linked list
+	loop do
+		node = l2
+		num2.concat(node.val.to_s)
+	  break if l2.next.nil?
+	  l2 = l2.next
+	end 
+
+	# reverse the numbers before adding
+	num1.reverse!
+	num2.reverse!
+
+	# add numbers
+	result = (num1.to_i + num2.to_i).to_s
+
+	# reverse numbers to place them in a linked list again
+	result.reverse!
+	sum_values = result.split('')
+
+	# create the first one, remove from sum_values, then loop through remainder (if any)
+	linked_list_sum = ListNode.new(sum_values.first)
+	linked_list_head = linked_list_sum # save head for returning
+	sum_values.shift # remove first val
+
+	# loop through remaining values
+	sum_values.each do |val|
+		linked_list_sum.next = ListNode.new(val)
+		linked_list_sum = linked_list_sum.next
+	end
+
+	return linked_list_head
+end
+
+add_two_numbers(l1, l2)
