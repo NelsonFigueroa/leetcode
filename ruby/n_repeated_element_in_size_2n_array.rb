3427747a61cd98f8b7147015fabd88e5c2d2e881
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-09-01 20:25:24 -0700

    completed new challenge

---
+++ ruby/n_repeated_element_in_size_2n_array.rb
@@ -0,0 +1,27 @@
+# Ruby 2.6.3
+# https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+# array for testing
+a = [1,2,3,3]
+
+def repeated_n_times(a)
+	element_counts = {}
+
+	a.each do |element|
+		if element_counts.key?(element)
+			# if key already exists, increment the count (value)
+			element_counts[element] = element_counts[element] + 1
+		else
+			# if key doesn't exist, add it to hash with count 1
+			element_counts[element] = 1
+		end
+	end
+
+	# return the element that is repeated
+	# check .values of hash and find the .max value
+	element_counts.each { |element, count| return element if count == element_counts.values.max }
+end
+
+answ = repeated_n_times(a)
+
+puts answ
