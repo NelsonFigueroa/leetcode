f3d3dcb5d58cc58c98d1e4d81c6d08d182d29f25
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-09-10 00:36:54 -0700

    completed univalued tree challenge

---
+++ ruby/univalued_binary_tree.rb
@@ -0,0 +1,34 @@
+# Ruby 2.6.3
+# https://leetcode.com/problems/univalued-binary-tree/
+
+# Node definition
+class TreeNode
+	attr_accessor :val, :left, :right
+
+	def initialize(val)
+		@val = val
+		@left, @right = nil, nil
+	end
+end
+
+# Actual solution
+
+def is_unival_tree(root)
+	if root.nil?
+		return true
+	end
+
+	if (root.left != nil) && (root.left.val != root.val)
+		return false
+	end
+
+	if (root.right != nil) && (root.right.val != root.val)
+		return false
+	end
+
+	if is_unival_tree(root.left) && is_unival_tree(root.right)
+		return true
+	end
+
+	false
+end
