c30fb628d9fbbea30e49685fbe43cdedbe9f1371
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-09-17 21:17:05 -0700

    completed reverse string challenge

---
+++ ruby/reverse_string.rb
@@ -0,0 +1,23 @@
+# Ruby 2.6.3
+# https://leetcode.com/problems/reverse-string/
+
+def reverse_string(s)
+  # the easy solution:
+  # return s.reverse!
+
+  # the hard solution:
+  characters = s.split('')
+  reverse = []
+
+  length = characters.length
+
+  while (length >= 0) do
+    reverse << characters[length]
+    
+    length -= 1
+  end
+
+  return reverse.join('')
+end
+
+puts reverse_string("Hello")
