da0ccb5ab6a2580f0f8ba0f513d661d93fbaa918
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2019-09-19 14:44:51 -0700

    updated challenge, made progress

--- ruby/remove_all_adjacent_duplicates_in_string.rb
+++ ruby/remove_all_adjacent_duplicates_in_string.rb
@@ -1,28 +1,28 @@
 # Ruby 2.6.3
 # https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
-
 def remove_duplicates(s)
+  # puts "Current string: #{s}"
+  answer = s
   s = s.split('')
 
-  s.each_with_index do |char, index|
-    puts "Char: #{char}, Index: #{index}"
+  s.each_index do |index|
+    # puts "Char: #{char}, Index: #{index}"
 
     # make sure there is a previous char to inspect, don't go out of bounds
     # if current char is equal to previous char, delete both
-    if (index != 0) && (char == s[index - 1])
-      puts "Deleting Char: #{char} @ Index: #{index}"
-      puts "Deleting Char: #{s[index - 1]} @ Index: #{index - 1}"
+    if index == 0
+      next
+    elsif s[index] == s[index - 1]
+      # puts "Deleting Char: #{char} @ Index: #{index}"
+      # puts "Deleting Char: #{s[index - 1]} @ Index: #{index - 1}"
       s.delete_at(index)
       s.delete_at(index - 1)
-      puts s.join('')
-
-      remove_duplicates(s.join(''))
+      # puts "Resulting string: #{s.join('')}"
+      
+      answer = remove_duplicates(s.join(''))
     end
   end
 
-  # Maybe i dont understand how recursion works
-  # this is returning 3 times!
-  # the recursion is 3 times thats why
-  puts "Returning array"
-  return s.join('')
+  # puts "Returning array"
+  return answer
 end
