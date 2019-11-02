083fdd989d3f631fc69e4f9e1ffda46a0b9c7474
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-11-01 19:06:25 -0700

    completed first challenge in Go

---
+++ go/fibonacci_number.go
@@ -0,0 +1,20 @@
+// Go 1.13.3
+// https://leetcode.com/problems/fibonacci-number/
+
+package main
+
+import (
+	"fmt"
+)
+
+func fib(N int) int {
+	if N == 1 || N == 0 {
+		return N
+	}
+
+	return (fib(N-1) + fib(N-2))
+}
+
+func main() {
+	fmt.Println(fib(5))
+}
