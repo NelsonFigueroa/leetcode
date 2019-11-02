cbdde1e999e37e8e290b063eaa09da51598f7f62
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-11-01 20:34:20 -0700

    completed new challenge

---
+++ go/day_of_the_week.go
@@ -0,0 +1,21 @@
+// Go 1.13.3
+// https://leetcode.com/problems/day-of-the-week/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func dayOfTheWeek(day int, month int, year int) string {
+
+	date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+	result := date.Weekday().String()
+
+	return result
+}
+
+func main() {
+	fmt.Println(dayOfTheWeek(31, 10, 2019))
+}
