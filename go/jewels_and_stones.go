593e2905c6af84696a84e924aaeb10d09278dc2e
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-11-04 00:29:21 -0800

    fixed formatting

--- go/jewels_and_stones.go
+++ go/jewels_and_stones.go
@@ -4,31 +4,31 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+  "fmt"
+  "strings"
 )
 
 func numJewelsInStones(J string, S string) int {
 
-	jewel_count := 0
+  jewel_count := 0
 
-	// split strings into array of chars
-	jewels := strings.Split(J, "")
-	stones := strings.Split(S, "")
+  // split strings into array of chars
+  jewels := strings.Split(J, "")
+  stones := strings.Split(S, "")
 
-	// loop through each stone
-	for s_index := range stones {
-		// check if stone exists in jewels array
-		for j_index := range jewels {
-			if stones[s_index] == jewels[j_index] {
-				jewel_count++
-			}
-		}
-	}
+  // loop through each stone
+  for s_index := range stones {
+    // check if stone exists in jewels array
+    for j_index := range jewels {
+	    if stones[s_index] == jewels[j_index] {
+	      jewel_count++
+	     }
+     }
+  }
 
-	return jewel_count
+  return jewel_count
 }
 
 func main() {
-	fmt.Println(numJewelsInStones("aA", "aAAbbbb"))
+  fmt.Println(numJewelsInStones("aA", "aAAbbbb"))
 }
