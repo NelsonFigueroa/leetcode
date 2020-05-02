e7499fdd0d664e6ed18943a3994d3c88600dd0fd
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-05-02 13:53:35 -0700

    slightly faster

--- ruby/jewels_and_stones.rb
+++ ruby/jewels_and_stones.rb
@@ -1,9 +1,8 @@
-# Ruby 2.5.3
 # https://leetcode.com/problems/jewels-and-stones/
 
 def num_jewels_in_stones(j, s)
-  jewels = j.to_s.split('')
-  stones = s.to_s.split('')
+  jewels = j.chars
+  stones = s.chars
 
   counter = 0
   stones.each do |stone|
