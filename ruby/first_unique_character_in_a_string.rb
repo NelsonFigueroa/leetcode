268687df4bc12d42b2be8bf73d2c7cfaefa422a8
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-05-05 02:54:20 -0700

    new challenge complete

---
+++ ruby/first_unique_character_in_a_string.rb
@@ -0,0 +1,12 @@
+def first_uniq_char(s)
+  return -1 if s.empty?
+
+  s = s.chars
+  chars = s.uniq
+  first_unique_char = chars.reject { |char| s.count(char) > 1 }.first
+
+  index = s.index(first_unique_char)
+  return -1 if index.nil?
+
+  index
+end
