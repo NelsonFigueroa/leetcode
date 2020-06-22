f9b4d870d89c765a996c1b504b236a234c1c5f52
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-06-21 21:30:06 -0700

    completed defanging an ip in Golang

---
+++ go/defanging_an_ip_address.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/defanging-an-ip-address/submissions/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func defangIPaddr(address string) string {
+	// split by dot, then join by [.]
+	return strings.Join(strings.Split(address, "."), "[.]")
+}
+
+func main() {
+	fmt.Println(defangIPaddr("192.168.1.1"))
+}
