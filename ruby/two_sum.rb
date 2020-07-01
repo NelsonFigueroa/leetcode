0bcd82e5f57d19170d4cd8cc834d84202799240d
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-06-30 17:45:21 -0700

    better answer

--- ruby/two_sum.rb
+++ ruby/two_sum.rb
@@ -1,15 +1,11 @@
 # https://leetcode.com/problems/two-sum/
 
 def two_sum(nums, target)
-  nums.each_with_index do |num1, index1|
-    nums.each_with_index do |num2, index2|
-      # skip if the index is the same, cant use the same index twice
-      unless (index1 == index2)
-        if (num1 + num2) == target
-          puts "Indices #{index1} #{index2}"
-          return [index1, index2]
-        end
-      end
-    end
+  nums.each_with_index do |num, index|
+    second_num = target - num
+    next unless nums.include?(second_num)
+
+    second_index = nums.index(second_num)
+    return [index, second_index] if index != second_index
   end
 end
