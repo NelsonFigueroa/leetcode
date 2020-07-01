0064cfdef8e92cd539bced4954009dae3279ed09
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-06-30 17:59:21 -0700

    linting

--- ruby/string_to_integer.rb
+++ ruby/string_to_integer.rb
@@ -2,29 +2,26 @@
 
 # https://leetcode.com/problems/string-to-integer-atoi/
 
-INT_MIN = -2147483648
-INT_MAX = 2147483647
+INT_MIN = -2_147_483_648
+INT_MAX = 2_147_483_647
 
 def atoi(string)
-
   # Initialize integer to be returned
   integer = ''
 
-  # Remove leading whitespace 
+  # Remove leading whitespace
   string = string.lstrip
 
-  # If first character is not a number, or +/-, return 0, 
+  # If first character is not a number, or +/-, return 0,
   if /[^0-9]/.match(string[0])
     if string[0] != '+'
-      if string[0] != '-'
-        return 0
-      end
+      return 0 if string[0] != '-'
     end
   end
 
   # Check if first char is + or -
   if string[0] == '+' || string[0] == '-'
-    # If it is, add to integer << 
+    # If it is, add to integer <<
     integer << string[0]
     # Remove it from the original string
     string = string[1..-1]
@@ -44,14 +41,10 @@ def atoi(string)
   end
 
   # If integer is below minimum 32-bit value, return INT_MIN
-  if integer.to_i < INT_MIN.to_i
-    return INT_MIN
-  end
+  return INT_MIN if integer.to_i < INT_MIN.to_i
 
   # If integer is greater than maximum 32-bit value, return INT_MAX
-  if integer.to_i > INT_MAX.to_i
-    return INT_MAX
-  end
+  return INT_MAX if integer.to_i > INT_MAX.to_i
 
-  return integer.to_i
+  integer.to_i
 end
