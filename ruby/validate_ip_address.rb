a6e200c0d9fb09b052d8ed33ac20526692564c99
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-06-30 17:53:16 -0700

    linting

--- ruby/validate_ip_address.rb
+++ ruby/validate_ip_address.rb
@@ -1,24 +1,19 @@
-# Ruby 2.5.0
-
 # https://leetcode.com/problems/validate-ip-address/
 
 def valid_ip_address(ip)
-
   # Check if input is in IPv4 format
   if /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(ip)
-    
+
     ip.split('.').each do |octet|
       # Octets must be within 0-255 range
-      if octet.to_i > 255 || octet.to_i < 0
-        return "Neither"
-      end
+      return 'Neither' if octet.to_i > 255 || octet.to_i < 0
 
       # Octets should not have leading zeroes
       if /0{1,2}\d*/.match(octet)
-        return "Neither" unless octet == '0' # don't return 'neither' if it's a single zero.
+        return 'Neither' unless octet == '0' # don't return 'neither' if it's a single zero.
       end
     end
-    return "IPv4"
+    return 'IPv4'
   end
 
   # Check if input is in IPv6 format
@@ -26,13 +21,11 @@ def valid_ip_address(ip)
 
     ip.split(':').each do |segment|
       # Segments must be between 0 - FFFF
-      if segment.hex > 65535 || segment.hex < 0
-        return "Neither"
-      end
+      return 'Neither' if segment.hex > 65_535 || segment.hex < 0
     end
-    return "IPv6"
+    return 'IPv6'
   end
 
   # No matches to IPv4 or IPv6
-  return "Neither"
+  'Neither'
 end
