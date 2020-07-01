9a6866d0de36c4cd167f1a49ee8c5804724c84d4
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-06-30 17:46:07 -0700

    new ruby challenge

---
+++ ruby/rotate_array.rb
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/rotate-array/submissions/
+
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+  k.times do
+    last = nums.last
+    nums.pop
+    nums.unshift(last)
+  end
+end
