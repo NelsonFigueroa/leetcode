96b23857c94fad670be60cf581a0175ccb1d6c40
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-07-04 03:12:42 -0700

    alternate solution

--- ruby/squares_of_a_sorted_array.rb
+++ ruby/squares_of_a_sorted_array.rb
@@ -1,5 +1,15 @@
 # https://leetcode.com/problems/squares-of-a-sorted-array
 
+# cool answer
+# def sorted_squares(a)
+#   a.map! { |num| num**2 }.sort
+# end
+
+# slightly faster
 def sorted_squares(a)
-  a.map! { |num| num ** 2 }.sort
+  a.each_index do |i|
+    a[i] = a[i]**2
+  end
+
+  a.sort
 end
