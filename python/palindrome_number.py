64eeae338d4a7dc5517e521e49bbdb57206c517a
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-02-01 00:06:46 -0800

    palindrome number in python

---
+++ python/palindrome_number.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int):
+        if x < 0:
+            return False
+
+        x = str(x)
+        reverse = x[::-1]
+
+        if x == reverse:
+            return True
+
+        return False
+        
+result = Solution.isPalindrome(Solution, 121)
+print(result)
