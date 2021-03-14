3f3b4933c34eb50b8be47026948e188ad4c95cac
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-03-14 15:04:41 -0700

    more types

--- python/find_numbers_with_even_number_of_digits.py
+++ python/find_numbers_with_even_number_of_digits.py
@@ -1,7 +1,9 @@
 # https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
+from typing import List
+
 class Solution:
-    def findNumbers(self, nums):
+    def findNumbers(self, nums: List[int]) -> int:
         even_numbers = 0
         for num in nums:
             if len(str(num)) % 2 == 0:
